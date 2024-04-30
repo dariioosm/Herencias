b1978550a5d174ac89a9b415551187cf2f76071d
dariioosm <dariosanchez351@gmail.com>
2024-04-30 17:15:32 +0200

    correccion de las clases Titula y BancoAviles

--- src/BancoAviles/BancoAviles.java
+++ src/BancoAviles/BancoAviles.java
@@ -1,36 +1,5 @@
 package BancoAviles;
 
 public class BancoAviles {
-    private String nombre;
-    private String apellido;
-    private int edad;
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    @Override
-    public String toString() {
-        return "BancoAviles [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + "]";
-    }
 }
