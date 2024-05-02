03ba35cd45fd8b0fb825a11ad5cc0628a860b656
dariioosm <dariosanchez351@gmail.com>
2024-05-02 15:44:55 +0200

    setter y getter de clase padre productos

---
+++ src/productos/Producto.java
@@ -0,0 +1,36 @@
+package productos;
+
+public class Producto {
+    protected String nombre;
+    protected String codigo;
+    protected int precio;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public Producto() {
+        super();
+    }
+
+}
