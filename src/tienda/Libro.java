0f3a4ada783e79ea0b2cbf4f6ddab9e236435ad0
dariioosm <dariosanchez351@gmail.com>
2024-05-06 16:35:01 +0200

    creacion del metodo annadir libro

--- src/tienda/Libro.java
+++ src/tienda/Libro.java
@@ -1,5 +1,8 @@
 package tienda;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Libro extends Articulo {
     private int isbn;
     private int numPag;
@@ -33,11 +36,46 @@ public class Libro extends Articulo {
         super();
     }
 
-    public Libro(int referencia, String nombre, int precio, int numPag, String autor) {
+    public Libro(int referencia, String nombre, int precio, int numPag, int isbn, String autor) {
         super(referencia, nombre, precio);
         this.numPag = numPag;
         this.isbn = isbn;
         this.autor = autor;
+    }
+
+    public void anadirLibro() {
+        // donde crear el arraylist?
+        Scanner in = new Scanner(System.in);
+        char respuesta = 's';
+        try {
+            ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream("registroLibro.dat"));
+            do {
+                // ordenes para el usuario
+                System.out.println("Introduzca referencia del producto: ");
+                String referencia = in.nextLine();
+                System.out.println("Introduzca nombre del libro: ");
+                String nombre = in.nextLine();
+                System.out.println("Introduzca precio del libro: ");
+                int precio = in.nextInt();
+                System.out.println("Introduzca numero de paginas: ");
+                int numPag = in.nextInt();
+                System.out.println("Introduzca numero ISBN: ");
+                int isbn = in.nextInt();
+                System.out.println("Introduzca los apellidos y nombre del autor: ");
+                String autor = in.nextLine();
+                // creacion del objeto
+                lib = new Libro();
+                // escribir el objeto al arraylist
+
+                // escribir el objeto en el fichero
+
+            } while (Character.toLowerCase(respuesta) != 'n');
+        } catch (FileNotFoundException ex) {
+            System.out.println("Fichero no encontrado" + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("Error en la entrada o salida de datos " + ex.getMessage());
+        }
+
     }
 
     @Override
