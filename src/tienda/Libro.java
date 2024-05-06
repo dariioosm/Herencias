ac409cc5ca6dfc6076e7516653266671a13baef3
dariioosm <dariosanchez351@gmail.com>
2024-05-06 15:27:54 +0200

    getter y setter Libro

--- src/tienda/Libro.java
+++ src/tienda/Libro.java
@@ -1,5 +1,37 @@
 package tienda;
 
 public class Libro {
-    
+    private int isbn;
+    private int numPag;
+    private String autor;
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(int numPag) {
+        this.numPag = numPag;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Libro -> ISBN: " + isbn + ", NumPaginas: " + numPag + ", Autor: " + autor;
+    }
+
 }
