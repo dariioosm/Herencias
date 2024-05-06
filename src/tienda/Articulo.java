6a3ed7f7b4a8406814706f51b33d1ff6c8566366
dariioosm <dariosanchez351@gmail.com>
2024-05-06 14:59:07 +0200

    getters y setters de Articulo

--- src/tienda/Articulo.java
+++ src/tienda/Articulo.java
@@ -1,5 +1,41 @@
 package tienda;
 
 public class Articulo {
-    
+    protected int referencia;
+    protected String nombre;
+    protected int precio;
+
+    public int getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(int referencia) {
+        this.referencia = referencia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    // constructor vacio
+    public Articulo() {
+        super();
+    }
+
+    // constructor parametros
+    public Articulo(int referencia, String nombre, int precio) {
+        super();
+    }
 }
